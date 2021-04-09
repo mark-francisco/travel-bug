6a480dd80e102e171182e7e64a7dd6288d3825cd
Mark Francisco <mfrancisco581@gmail.com>
2021-04-09 15:02:29 -0500

    Add Trips index routes, controller actions, and views

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,2 +1,18 @@
 class Api::TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+    render "index.json.jb"
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,12 @@ Rails.application.routes.draw do
     post "/stops" => "stops#create"
     patch "/stops/:id" => "stops#update"
     delete "/stops/:id" => "stops#destroy"
+
+    # TRIPS
+    get "/trips" => "trips#index"
+    get "/trips/:id" => "trips#show"
+    post "/trips" => "trips#create"
+    patch "/trips/:id" => "trips#update"
+    delete "/trips/:id" => "trips#destroy"
   end
 end
