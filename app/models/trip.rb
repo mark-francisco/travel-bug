7cab855f78139f5e4dcc7d7a9055062226ef051c
Mark Francisco <mfrancisco581@gmail.com>
2021-04-14 17:57:14 -0400

    Add has_many :through association from Trip to Destination

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,4 +7,14 @@ class Trip < ApplicationRecord
 
   belongs_to :owner, :class_name => "User", :inverse_of => "trips_owned"
   belongs_to :collaborator, :class_name => "User", :inverse_of => "trips_collaborated", :optional => true
+
+  # A Trip can have many Destinations
+  has_many :destinations, :through => :stops
+  # def destinations
+  #   stops = Stop.where(:trip_id => id)
+
+  #   destinations = stops.map { |stop|
+  #     Destination.find_by(:id => stop.destination_id)
+  #   }
+  # end
 end
