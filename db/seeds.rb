610f952f92d69fa9c5d08491973946854326313c
Mark Francisco <mfrancisco581@gmail.com>
2021-05-05 14:19:01 -0500

    Seed Destination data (all world cities with population of 1+ mil

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,63 +2,47 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
+# require "csv" library so that you can import Destination data
+require "csv"
+
+User.delete_all
+Destination.delete_all
+Trip.delete_all
+Stop.delete_all
+
+# https://levelup.gitconnected.com/quickly-seeding-a-rails-database-using-a-csv-file-8438b8b68f05
+CSV.foreach(Rails.root.join("lib/seed-data-from-simple-maps/worldcities-1mil-and-up.csv"), :headers => true) do |row|
+  Destination.create!({
+    :name => row["city_ascii"],
+    :country => row["country"],
+    :lat => row["lat"],
+    :lng => row["lng"],
+  })
+end
+
 # Travel Bug seed data
-# User.create!(:first_name => "Mark", :last_name => "Francisco", :username => "mark", :email => "m@example.com", :password => "password")
-# User.create!(:first_name => "Test", :last_name => "User", :username => "username2", :email => "t@example.com", :password => "password")
+User.create!(:first_name => "Mark", :last_name => "Francisco", :username => "mark", :email => "m@example.com", :password => "password")
+User.create!(:first_name => "Jamie", :last_name => "Jones", :username => "jonesy", :email => "j@example.com", :password => "password")
 
-# Destination.create(
-#   :country => "US",
-#   :name => "Chicago",
-#   :lat => "41.85003",
-#   :lng => "-87.65005",
-# )
-# Destination.create(
-#   :country => "US",
-#   :name => "San Diego",
-#   :lat => "32.71571",
-#   :lng => "-117.16472",
-# )
-# Destination.create(
-#   :country => "CA",
-#   :name => "Vancouver",
-#   :lat => "49.24966",
-#   :lng => "-123.11934",
-# )
+Trip.create!(
+  :title => "Olympic National Park",
+  :owner_id => 1,
+  :collaborator_id => 2,
+  :description => "birthplace of celery",
+  :isComplete => true,
+  :logistics => ["rent a car and ferry over", "leave no trace"],
+  :ideas => ["couple days here", "couple days there", "couple days everywhere"],
+)
 
-# Trip.create(
-#   :title => "US west coast",
-#   :owner_id => 1,
-#   :collaborator_id => 2,
-#   :description => "gonna have a good time for a bit",
-#   :isComplete => false,
-#   :logistics => ["fly in on 7/1", "bring your passport", "rent a car"],
-#   :ideas => ["go camping in canada", "go to that sourdough place", "chill for a couple days at XYZ"],
-# )
-# Trip.create(
-#   :title => "US city break",
-#   :owner_id => 2,
-#   :collaborator_id => 3,
-#   :description => "birthplace of celery",
-#   :isComplete => true,
-#   :logistics => ["fly into Midway", "stay not downtown", "fly out on Sunday AM"],
-#   :ideas => ["spend 3 days at The Bean", "do touristy activities", "go to a few museums"],
-# )
-
-# Stop.create(
-#   :trip_id => 1,
-#   :destination_id => 2,
-#   :start_date => DateTime.new(2021, 7, 1),
-#   :end_date => DateTime.new(2021, 7, 7),
-# )
-# Stop.create(
-#   :trip_id => 1,
-#   :destination_id => 3,
-#   :start_date => DateTime.new(2021, 7, 7),
-#   :end_date => DateTime.new(2021, 7, 13),
-# )
-# Stop.create(
-#   :trip_id => 2,
-#   :destination_id => 1,
-#   :start_date => DateTime.new(2019, 8, 2),
-#   :end_date => DateTime.new(2019, 8, 7),
-# )
+Stop.create!(
+  :trip_id => 1,
+  :destination_id => 2,
+  :start_date => DateTime.new(2021, 7, 1),
+  :end_date => DateTime.new(2021, 7, 7),
+)
+Stop.create!(
+  :trip_id => 1,
+  :destination_id => 3,
+  :start_date => DateTime.new(2021, 7, 1),
+  :end_date => DateTime.new(2021, 7, 7),
+)
